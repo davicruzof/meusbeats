b3542c9500921877dd87ccf0ecd44aa2e08b7725
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2021-11-22 14:08:22 -0300

    fix(textiput): Editando o componente para deixar dinamico

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,27 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, ContainerLabel, Input, Label } from './styles';
 
-export const TextInput = ({ color, label ,onBlur,onFocus, active, ...rest }) => {
+export const TextInput = ({ color, placeholder, label ,onBlur,onFocus, active, ...rest }) => {
   
   const [isFocused, setIsFocused] = useState(false);
+  const [border, setBorder] = useState(false);
+
+  useEffect(() => {
+    if(isFocused && active){
+      setBorder(true)
+    }else{
+      if(isFocused && !active){
+        setBorder(true)
+      }else{
+        setBorder(false)
+      }
+    }
+  },[isFocused])
 
   return (
     <Container>
       <Input
-        onBlur={(event) => {
-          setIsFocused(active)
-          onBlur?.(event)
-        }}
-        onFocus={(event) => {
-          setIsFocused(true)
-          onFocus?.(event)
-        }}
-        focused={isFocused}
+        onBlur={() => setIsFocused(false)}
+        onFocus={() => setIsFocused(true)}
+        focused={border}
         {...rest}
+        placeholder={isFocused ? "" : placeholder}
       />
-      <ContainerLabel color={color} focused={active || isFocused}>
-        <Label focused={isFocused} >{label}</Label>
-      </ContainerLabel>
+      {
+        (isFocused || active) && (
+          <ContainerLabel color={color} active={active} focused={isFocused}>
+            <Label focused={isFocused} active={active} >{label}</Label>
+          </ContainerLabel>
+        )
+      }
     </Container>
   );
 };
