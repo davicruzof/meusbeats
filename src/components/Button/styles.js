6ca15ff80a7b6075225b9a049b5a8215c65b9615
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2021-11-22 16:11:58 -0300

    create button

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.TouchableOpacity`
+    background-color: #6200EE;
+    height: 50px;
+    border-radius: 200px;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const TextButton = styled.Text`
+    color: #fff;
+    font-family: 'DMSans-Bold';
+    font-weight: bold;
+    font-size: 16px;
+    letter-spacing: 0.2px;
+`;
