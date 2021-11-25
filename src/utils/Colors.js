9f6acef2ef6afdf67b3abfcc749af28dcd74598f
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2021-11-25 11:20:45 -0300

    style(Font / Colors): Adicionando cores e fonts
    Adicionando cores e fontes em arquivos globais para melhor manutenção e utilização, como tambem sua escalabilidade

---
+++ src/utils/Colors.js
@@ -0,0 +1,13 @@
+export const Colors = {
+    white: '#fff',
+    black: '#000',
+    gray: '#DADADA',
+    green: '#0ACF83',
+    lilac: '#BB86FC',
+    primary: '#985EFF',
+    darkGray: '#979797',
+    secondary: '#6200EE',
+    darkGreen: '#019592',
+    transparent: 'rgba(0,0,0,0)',
+    placeholder: 'rgba(255,255,255,0.6)'
+};
