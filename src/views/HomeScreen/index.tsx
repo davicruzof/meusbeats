3bf90dd32e4910c0a2e72dee6e87caf776c76c50
davicruzof <daviondejesus@gmail.com>
2021-12-22 14:40:53 -0300

    refactor(imports): Ajustando imports

--- src/views/HomeScreen/index.tsx
+++ src/views/HomeScreen/index.tsx
@@ -1,7 +1,8 @@
-import { Header } from '@components/Header';
-import { Colors } from '@utils/Colors';
 import React, { ReactNode } from 'react';
 import { StatusBar } from 'react-native';
+
+import { Header } from '@components/Header';
+import { Colors } from '@utils/Colors';
 
 import { Container } from './styles';
 
