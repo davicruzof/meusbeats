acd1a276745f49e289e3584fe92fc153c22af26e
davicruzof <daviondejesus@gmail.com>
2021-12-23 11:57:21 -0300

    refactor(ui): Ajustando gradiente da header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,9 +11,12 @@ function Header({ children }: HeaderProps): ReactElement {
         <Background
             size={377}
             locations={[0, 0]}
-            colors={[Colors.ligthMediumPrimary, Colors.darkPrimary]}
+            colors={[Colors.purple, Colors.darkPrimary]}
         >
-            <Background colors={[Colors.transparent, Colors.black]}>
+            <Background 
+                size={377}
+                locations={[0.8, 1]}
+                colors={[Colors.transparent, Colors.black]}>
                 {children}
             </Background>
         </Background>
